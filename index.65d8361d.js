8d1c81a9cb687fcdbf56f05dc0c367775b6b7604
vitalii-khymchuk <103372377+vitalii-khymchuk@users.noreply.github.com>
2022-07-17 08:52:50 +0000

    Deploying to gh-pages from @ vitalii-khymchuk/Ice-cream-project@ac2ae0c15948b8cfa57a4b8109632ba7642ac921 🚀

---
+++ index.65d8361d.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-made-open]"),closeModalBtn:document.querySelector("[data-modal-made-close]"),modal:document.querySelector("[data-modal-made]")};function d(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",d),e.closeModalBtn.addEventListener("click",d)}();
+//# sourceMappingURL=index.65d8361d.js.map
