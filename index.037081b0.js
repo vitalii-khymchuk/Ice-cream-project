72acfa0819ca2d826b5852e050e5af956a277815
vitalii-khymchuk <103372377+vitalii-khymchuk@users.noreply.github.com>
2022-07-17 20:57:40 +0000

    Deploying to gh-pages from @ vitalii-khymchuk/Ice-cream-project@793fa276b959efa98cdadc844fd0c36b74c82f99 🚀

--- index.037081b0.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper-container",{spaceBetween:35,slidesPerView:1,speed:800,autoplay:{delay:2e3},disableOnInteraction:!1,pauseOnMouseEnter:!0,loop:!0,paginationClickable:!0,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.037081b0.js.map
